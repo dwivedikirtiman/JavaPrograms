63f9f95606148e1b3af259d5d8001d10505e55ac
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-28 18:31:45 +0530

    Another  commit

---
+++ src/in/JavabasicPrograms/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+package in.JavabasicPrograms;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b =9;
+        int sum = a + b;
+        System.out.println("The sum of the given numbers are :" + sum);
+    }
+}
