ee80f55c1df2c53b47db44953ba51a5ce01393e9
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-28 18:36:20 +0530

    Another  commit

--- src/in/JavabasicPrograms/AddTwoNumbers.java
+++ src/in/JavabasicPrograms/AddTwoNumbers.java
@@ -1,10 +1,23 @@
 package in.JavabasicPrograms;
 
+import java.util.Scanner;
+
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int a = 5;
+/*        int a = 5;
         int b =9;
         int sum = a + b;
+        System.out.println("The sum of the given numbers are :" + sum);*/
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("This program will show the addition of the two number");
+        System.out.print("\nEnter the first number: ");
+        int num1 = input.nextInt();
+        System.out.print("\nEnter the second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
         System.out.println("The sum of the given numbers are :" + sum);
+
     }
 }
