7685ba8e4baf033ed324f70bc94747ab9fcb1edd
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-03-03 22:40:54 +0530

    Another  commit

---
+++ src/in/JavabasicPrograms/FibonacciByRecursiveWay.java
@@ -0,0 +1,30 @@
+package in.JavabasicPrograms;
+import java.util.Scanner;
+public class FibonacciByRecursiveWay {
+        public static void main(String[] args) {
+            System.out.println("This program shows the Fibonacci series up to the given number of terms.");
+            Scanner input = new Scanner(System.in);
+            System.out.print("\nEnter the number of terms for the Fibonacci series: ");
+            int term = input.nextInt();
+
+            System.out.println("Fibonacci series:");
+            for (int i = 0; i < term; i++) {
+                System.out.print(fibonacci(i)+ " ");
+            }
+
+            input.close();
+        }
+
+        // Recursive function to calculate Fibonacci number
+        public static int fibonacci(int n) {
+            if (n <= 0) {
+                return 0;
+            } else if (n == 1) {
+                return 1;
+            } else {
+                return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+        }
+}
+
+
