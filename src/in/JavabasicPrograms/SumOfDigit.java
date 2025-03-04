4a45b350fc3f9f17ba28afdd68bbce8fa1ef2a12
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-03-04 18:02:43 +0530

    Another  commit

---
+++ src/in/JavabasicPrograms/SumOfDigit.java
@@ -0,0 +1,22 @@
+package in.JavabasicPrograms;
+
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("This program will add the digits of the given number");
+        System.out.print("\nEnter the number: ");
+        int num = input.nextInt();
+        int sum=0;
+        for (; num > 0; num /= 10) {
+            sum += num % 10;  // Extract last digit and add to sum
+        }
+        /*while(num>0){
+            int remainder=num%10;
+            sum = sum+remainder;
+            num= num/10;
+        }*/
+        System.out.println("The sum of the digits is "+ sum);
+    }
+}
